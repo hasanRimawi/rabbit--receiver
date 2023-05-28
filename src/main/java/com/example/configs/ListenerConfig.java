e03e79e9c6b938b77c5e34b5f542e31e0426cfe0
hasanRimawi <1183268@student.birzeit.edu>
2023-05-28 09:17:22 +0300

    added second queue - threads' pool to receive messages concurrently

--- src/main/java/com/example/configs/ListenerConfig.java
+++ src/main/java/com/example/configs/ListenerConfig.java
@@ -1,6 +1,7 @@
 package com.example.configs;
 
-
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
@@ -8,10 +9,12 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import com.example.services.RabbitConsumer;
+//import com.example.services.RabbitConsumer;
+
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
-
 
 @Configuration
 public class ListenerConfig {
@@ -29,26 +32,67 @@ public class ListenerConfig {
 //	  MessageListenerAdapter listenerAdapter(RabbitConsumer receiver) {
 //	    return new MessageListenerAdapter(receiver, "receiveMessage");
 //	  }
-	@Bean
+	@Bean(name = "firstQueue")
 	Queue queue() {
 		return new Queue("spring-boot", false);
 	}
+
+	@Bean(name = "secondQueue")
+	Queue queue2() {
+		return new Queue("summer-boot", false);
+	}
+
 	@Bean
 	ConnectionFactory connectionFactory() {
 		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
 		cachingConnectionFactory.setUsername("guest");
 		cachingConnectionFactory.setUsername("guest");
 		return cachingConnectionFactory;
-	}  
-	
-    //create MessageListenerContainer using custom connection factory
-	@Bean
-	MessageListenerContainer messageListenerContainer() {
-		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
-		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
-		simpleMessageListenerContainer.setQueues(queue());
-		simpleMessageListenerContainer.setMessageListener(new RabbitConsumer());
-		return simpleMessageListenerContainer;
-
 	}
+
+	// create MessageListenerContainer using custom connection factory assigns a class to handle messages
+//	@Bean
+//	MessageListenerContainer messageListenerContainer() {
+//		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+//		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+//		simpleMessageListenerContainer.setQueues(queue(), queue2());
+//		simpleMessageListenerContainer.setMessageListener(new RabbitConsumer());
+//		return simpleMessageListenerContainer;
+//
+//	}
+
+	@Bean
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory());
+		factory.setConcurrentConsumers(5); // Set the desired number of consumer threads
+		factory.setMaxConcurrentConsumers(10); // Set the maximum number of consumer threads
+		return factory;
+	}
+
+	@RabbitListener(queues = "spring-boot")
+	public void receiveMessage(Message message) {
+		try {
+			Thread.sleep(4 * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		System.out.println("retrieved from queue ONE -- " + Thread.currentThread().getId());
+		System.out.println("Listener Side (spring queue listener) Message was recieved: " + new String(message.getBody()) + "   "
+				+ message.getMessageProperties().getConsumerQueue() + "\n\n");
+//		System.out.println("Message is: " + message.getBody());
+	}
+	@RabbitListener(queues = "summer-boot")
+	public void receiveMessageQ2(Message message) {
+		try {
+			Thread.sleep(4 * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Listener Side (summer queue listener) Message was recieved: " + new String(message.getBody()) + "   "
+				+ message.getMessageProperties().getConsumerQueue() + "\n\n");
+	}
+
 }
--- src/main/java/com/example/services/RabbitConsumer.java
+++ src/main/java/com/example/services/RabbitConsumer.java
@@ -12,13 +12,25 @@ import org.springframework.stereotype.Service;
 //	public void received(String msg) {
 //		System.out.println("WORKED!!!  ->   " + msg);
 //	}
-@Service
-public class RabbitConsumer implements MessageListener{
-
-	@Override
-	public void onMessage(Message message) {
-		// TODO Auto-generated method stub
-		System.out.println("Message was recieved: " + new String(message.getBody()));
-	}
-	
-}
+//@Service
+//public class RabbitConsumer implements MessageListener{
+//
+//	@Override
+//	public void onMessage(Message message) {
+//		if(message.getMessageProperties().getConsumerQueue().compareTo("spring-boot2") == 0) {
+//			try {
+//				Thread.sleep(3 * 1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			System.out.println("retrieved from queue TWO -- " + Thread.currentThread().getId());
+//		}
+//		// TODO Auto-generated method stub
+//		else {
+//			System.out.println("retrieved from queue ONE -- " + Thread.currentThread().getId());
+//		}
+//		System.out.println("Message was recieved: " + new String(message.getBody()) + "   " + message.getMessageProperties().getConsumerQueue() + "\n\n");
+//	}
+//	
+//}
